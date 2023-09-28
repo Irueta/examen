32e289135ef968e7ac2c81b4282c946c271b7cfa
Irueta <irueta@gmail.com>
2023-09-28 12:13:23 +0200

    ejercicioSegundaOpcion

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //1
 
-function contarLetra(str, letra) {
+/* function contarLetra(str, letra) {
     str = str.toLowerCase();
     letra = letra.toLowerCase();
     let cuenta = 0;
@@ -10,16 +10,30 @@ function contarLetra(str, letra) {
       }
     }
     return cuenta;
-  }
+  } */
 
 //2   Diseña un algoritmo que imprima los números impares entre un número dado por el usuario y los siguientes 50 números
 
 const num = prompt("Introduce un número");
 function impar(num){
     let imprimir = "";
-    for(let i = num; i < num + 50; i++)
+    for(let i = num; i < num + 50; i++){
     if (i % 2 !== 0){
         imprimir += (i.toString()) + ", ";
     }
+}
+    return imprimir;
+}
+
+
+const num = prompt("Introduce un número");
+function impar(num){
+    const arr = [];
+    let imprimir = arr.join(', ');
+    for(let i = num; i < num + 50; i++){
+    if (i % 2 !== 0){
+        arr.push(i);
+    }
+}
     return imprimir;
 }
